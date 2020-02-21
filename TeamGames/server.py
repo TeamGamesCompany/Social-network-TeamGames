e96fe1a7bd12e7e94d25f46f7a027f983348c80f
Maxim Filonenko <56736526+maxim563328@users.noreply.github.com>
2020-02-21 21:01:30 +0700

    Создана структура сервера
    Теперь остаётся вроде только сделать регистрацию, базу данных, посты, переписки, группы

--- TeamGames/server.py
+++ TeamGames/server.py
@@ -1,1 +1,16 @@
-...
+from flask import Flask
+from flask_sslify import SSLify
+
+
+app = Flask(__name__)
+sslify = SSLify(app)
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+	with open('index.html', 'r') as file:
+		return file.read()
+
+
+if __name__ == '__main__':
+	app.run()
